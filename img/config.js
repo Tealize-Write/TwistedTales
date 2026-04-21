57d2ceca36b96b5c3533235a90683a220b246135
Tealize <58103246+Tealize-Write@users.noreply.github.com>
2026-04-21 19:20:20 +0800

    Add files via upload

---
+++ img/config.js
@@ -0,0 +1,6 @@
+/* ── CONFIG ── */
+const GAS_URL   = "https://script.google.com/macros/s/AKfycby0MAzFjOzkVex3lYxrvFjkhiujpd0HSelAHk76Wot_oOD_eOqkL2pKh20BAAfwyOPk/exec";
+const GAS_TOKEN = "rjgnlsfkhGgnlrwg2o7da$YGhf87eftocjdvlsfu";
+
+const ROMAN=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII'];
+
